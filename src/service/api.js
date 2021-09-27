063b731c48d5b2d2863afe14e61d8979b014e171
miyuki <5051099+miyukiair@users.noreply.github.com>
2021-09-27 11:44:58 +0800

    update dist

--- src/service/api.js
+++ src/service/api.js
@@ -3,17 +3,26 @@ import axios from 'axios';
 export const sendValidationCodePost = (name, email, token) => {
     const targetUrl = "https://script.google.com/macros/s/AKfycbxpx6GgQiGJYGeR0t8jO2NvTqnM02k8sinIHXGWlnqsx3tyYVWNlDBSUqiBdlKG_bAsYg/exec";
     let data = {name, email, token};
-    const request = axios.create({
-        headers: {
-            'Content-Type': 'application/x-www-form-encoded'
-        }
-    })
-    return request.post(targetUrl, data)
-        .then((response) => {
+    return axios.post(targetUrl, data, {
+        crossDomain: true
+    }).then(response => {
         if (response) {
             return response;
         } else {
             return Promise.reject();
         }
+    }).catch(error => {
+        console.log('error', error);
+    });
+};
+
+export const testGet = () => {
+    const targetUrl = "https://script.google.com/macros/s/AKfycbxpx6GgQiGJYGeR0t8jO2NvTqnM02k8sinIHXGWlnqsx3tyYVWNlDBSUqiBdlKG_bAsYg/exec";
+    return axios.get(targetUrl, {
+        crossDomain: true
+    }).then(res => {
+        console.log(res);
+    }).catch(error => {
+        console.log('error', error);
     })
 };
