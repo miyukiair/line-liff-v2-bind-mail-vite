5e49b67040b66a43ac78f2cfb89c2996f98b0226
miyuki <5051099+miyukiair@users.noreply.github.com>
2021-09-27 14:30:42 +0800

    update dist

--- src/service/api.js
+++ src/service/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-export const sendValidationCodePost = (name, email, token) => {
+export const sendValidationCodePost = (name, mail, token) => {
     const targetUrl = "https://script.google.com/macros/s/AKfycbxpx6GgQiGJYGeR0t8jO2NvTqnM02k8sinIHXGWlnqsx3tyYVWNlDBSUqiBdlKG_bAsYg/exec";
-    let data = JSON.stringify({name, email, token});
+    let data = JSON.stringify({name, mail, token});
     return axios.post(targetUrl, data, {
         headers: { 'content-type': 'application/x-www-form-urlencoded' }
     }).then(response => {
